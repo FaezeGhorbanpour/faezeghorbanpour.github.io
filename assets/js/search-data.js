c83963ea497d91125495fa18b2c4e4930f6da950
FaezeGhorbanpour <FaezeGhorbanpour@users.noreply.github.com>
2025-03-21 00:59:42 +0000

    Deploying to gh-pages from @ FaezeGhorbanpour/FaezeGhorbanpour.github.io@142632c7e9d43c36dd35ffcf18cd7c80304b94c1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,37 +44,10 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-      
-        title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-      
-      description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-      section: "Posts",
-      handler: () => {
-        
-          window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-        
-      },
-    },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-      
-        title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-      
-      description: "",
-      section: "Posts",
-      handler: () => {
-        
-          window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-        
-      },
-    },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-i-started-my-phd-at-cis-lmu-munich-under-the-supervision-of-prof-alex-fraser-and-dr-viktor-hangya-working-on-nlp-efficient-transfer-learning-and-hate-speech-detection",
+        },{id: "news-i-started-my-phd-at-cis-lmu-munich-under-the-supervision-of-prof-alex-fraser-and-dr-viktor-hangya-working-on-nlp-efficient-transfer-learning-and-hate-speech-detection",
           title: 'I started my PhD at CIS, LMU Munich under the supervision of Prof....',
           description: "",
-          section: "News",},{id: "news-our-research-paper-on-analyzing-communication-acts-during-crises-will-be-presented-at-asonam-2024-this-work-is-from-my-internship-at-aalto-university",
+          section: "News",},{id: "news-our-research-paper-on-analyzing-communication-acts-during-crises-will-be-presented-at-asonam-2024-this-work-is-from-my-internship-at-aalto-university-finaland",
           title: 'Our research paper on analyzing communication acts during crises will be presented at...',
           description: "",
           section: "News",},{id: "news-i-will-be-presenting-the-poster-for-label-specific-soft-prompt-tuning-at-the-2024-all-hands-meeting-of-the-national-ai-centers-in-dresden-germany",
@@ -86,45 +59,10 @@ ninja.data = [{
           section: "News",},{id: "news-our-paper-label-specific-representation-and-knowledge-transfer-for-harmful-content-detection-has-been-accepted-at-naacl-2025",
           title: '🎉 Our paper “Label-Specific Representation and Knowledge Transfer for Harmful Content Detection” has...',
           description: "",
-          section: "News",},{id: "news-our-paper-differentiating-emigration-and-return-migration-has-been-accepted-at-icwsm-2025-this-work-is-from-my-research-visit-at-mpidr",
+          section: "News",},{id: "news-our-paper-differentiating-emigration-and-return-migration-has-been-accepted-at-icwsm-2025-this-work-is-from-my-research-visit-at-mpidr-rostock-germany",
           title: '🎉 Our paper “Differentiating Emigration and Return Migration” has been accepted at ICWSM...',
           description: "",
           section: "News",},{
-        id: 'social-email',
-        title: 'email',
-        section: 'Socials',
-        handler: () => {
-          window.open("mailto:%66%61%65%7A%65.%67%68%6F%72%62%61%6E%70%6F%75%72@%74%75%6D.%64%65", "_blank");
-        },
-      },{
-        id: 'social-linkedin',
-        title: 'LinkedIn',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.linkedin.com/in/faeze-ghorbanpour/", "_blank");
-        },
-      },{
-        id: 'social-orcid',
-        title: 'ORCID',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://orcid.org/0009-0006-0570-4439", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=GQTf-pYAAAAJ&hl=en", "_blank");
-        },
-      },{
-        id: 'social-x',
-        title: 'X',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://twitter.com/FaezeGhorbanpor", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
